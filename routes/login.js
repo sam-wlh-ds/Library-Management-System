2b06b265f7ee82f57bceea2bd320e6298c1c2c6a
sam_wlh_ds <sam.wlh.ds@gmail.com>
2025-08-30 17:52:16 +0530

    Remove duplicate folders

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authController.js";
-
-const loginRouter = Router();
-
-loginRouter.post("/", authController.loginRoutePOST);
-
-export default loginRouter;
--- routes/register.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authController.js";
-
-const registerRouter = Router();
-
-registerRouter.post('/', authController.signupRoutePOST);
-
-export default registerRouter;
